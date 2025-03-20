2893db0e18a099c7910fc2efeb94c46cd815e32c
unknown <udayjagan16392@gmail.com>
2025-03-20 15:21:58 +0530

    added one problem from cognizant

---
+++ python_problems/problems/jump_ntimes.py
@@ -0,0 +1,12 @@
+n = int(input("n : ")) # length of list and output list
+arr = [int(input(f'arr index {i} :')) for i in range(n)] #given list
+k = int(input("k : ")) # jump size
+result = []
+cnt = 0
+index = 0
+while cnt < n :
+    result[cnt] = arr[index]
+    index = (index + k) % n
+    cnt += 1
+
+print(result)
